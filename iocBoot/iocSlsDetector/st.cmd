3e2780860d8d95f7c08f73cf46866bac423e719e
wang_x1 <wang_x1>
2012-06-22 12:04:13 +0000

    full setup

---
+++ iocBoot/iocSlsDetector/st.cmd
@@ -0,0 +1,34 @@
+< envPaths
+
+dbLoadDatabase("$(TOP)/dbd/slsDetectorApp.dbd")
+
+slsDetectorApp_registerRecordDeviceDriver(pdbbase)
+
+
+epicsEnvSet("PREFIX","13SD1:")
+epicsEnvSet("PORT",   "SD1")
+epicsEnvSet("QSIZE",  "20")
+epicsEnvSet("XSIZE",  "1280")
+epicsEnvSet("YSIZE",  "1")
+epicsEnvSet("NCHANS", "1360")
+
+slsDetectorConfig("SD1", "mcs6x18.config", 0, -1,-1)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/ADBase.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDFile.template",   "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(TOP)/slsDetectorApp/Db/slsDetector.template",        "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+
+
+NDStdArraysConfigure("Image1", 3, 0, $(PORT), 0)
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
+dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Float32,FTVL=FLOAT,NELEMENTS=10000")
+
+
+# Load all other plugins using commonPlugins.cmd
+< $(AREA_DETECTOR)/iocBoot/commonPlugins.cmd
+
+set_requestfile_path("$(TOP)/slsDetectorApp/Db")
+
+iocInit()
+
+# save things every thirty seconds
+create_monitor_set("auto_settings.req", 30,"P=$(PREFIX),D=cam1:")
---
+++ slsDetectorApp/slsDetectorSupport/detectorData.h
@@ -0,0 +1,39 @@
+
+#include <unistd.h>
+#include <cstring>
+#ifndef DETECTOR_DATA_H
+#define DETECTOR_DATA_H
+/**
+   @short data structure to hold the detector data after postprocessing 
+
+   (e.g. to plot, store in a root tree etc.)
+ */
+class detectorData {
+ public:
+  /** The constructor
+      \param val pointer to the data
+      \param err pointer to errors
+      \param ang pointer to the angles
+      \param f_ind file index
+      \param fname file name to which the data are saved
+      \param np number of points in x coordinate defaults to the number of detector channels (1D detector)
+      \param ny dimension in y (1D detector)
+  */
+  detectorData(float *val=NULL, float *err=NULL, float *ang=NULL,  float p_ind=-1, const char *fname="", int np=-1, int ny=1) : values(val), errors(err), angles(ang),  progressIndex(p_ind), npoints(np), npy(ny){strcpy(fileName,fname);};
+    /** 
+	the destructor
+	deletes also the arrays pointing to data/errors/angles if not NULL
+    */
+    ~detectorData() {if (values) delete [] values; if (errors) delete [] errors; if (angles) delete [] angles;};
+    //private:
+    float *values; /**< pointer to the data */
+    float *errors; /**< pointer to the errors */
+    float *angles;/**< pointer to the angles */
+    float progressIndex;/**< file index */
+    char fileName[1000];/**< file name */
+    int npoints;/**< number of points */
+    int npy;/**< dimensions in y coordinate*/
+};
+
+
+#endif
