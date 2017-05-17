e0a5fa814950c9433e24a5b0a42b4c60e669aa51
helfenstein_p <patrick.helfenstein@psi.ch>
2017-05-17 11:49:59 +0200

    fix template search path

--- iocs/jungfrauIOC/iocBoot/iocJungfrau/st.cmd
+++ iocs/jungfrauIOC/iocBoot/iocJungfrau/st.cmd
@@ -4,7 +4,7 @@ dbLoadDatabase("$(TOP)/dbd/jungfrauApp.dbd")
 
 jungfrauApp_registerRecordDeviceDriver(pdbbase)
 
-
+epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/ADApp/Db")
 epicsEnvSet("PREFIX","X09LB-SD1:")
 epicsEnvSet("PORT",   "SD1")
 epicsEnvSet("QSIZE",  "20")
@@ -22,17 +22,33 @@ epicsEnvSet("NCHANS", "2048")
 #               maxMemory)      # The maximum amount of memory that the NDArrayPool for this driver is 
 #                                 allowed to allocate. Set this to -1 to allow an unlimited amount of memory.
 slsDetectorConfig("SD1", "bchip039.config", 0, 0, -1,-1)
-dbLoadRecords("$(TOP)/slsDetectorApp/Db/slsDetector.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
+dbLoadRecords("$(slsDetector)/slsDetectorApp/Db/slsDetector.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin
 NDStdArraysConfigure("Image1", 3, 0, "$(PORT)", 0)
-dbLoadRecords("$(AREA_DETECTOR)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0,TYPE=Float64,FTVL=DOUBLE,NELEMENTS=524288")
+dbLoadRecords("$(ADCORE)/ADApp/Db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0,TYPE=Float64,FTVL=DOUBLE,NELEMENTS=524288")
 
+# Create a ZeroMQ server to publish NDArray
+# NDZMQConfigure (
+#     portName,          # The name of the asyn port driver to be created.
+#     serverHost,        # The ZMQ server to be created.
+#     queueSize,         # The number of NDArrays that the input queue for this plugin can hold when
+#                          NDPluginDriverBlockingCallbacks=0.
+#     blockingCallbacks, # 0=callbacks are queued and executed by the callback thread; 1 callbacks execute in the thread
+#                          of the driver doing the callbacks.
+#     NDArrayPort,       # Port name of NDArray source
+#     NDArrayAddr,       # Address of NDArray source
+#     maxBuffers,        # Maximum number of NDArray buffers driver can allocate. -1=unlimited
+#     maxMemory)         # Maximum memory bytes driver can allocate. -1=unlimited
+NDZMQConfigure("NDZMQ1", "tcp://*:1234", 3, 0, "SD1", 0, -1, -1)
+dbLoadRecords("$(ADCORE)/ADApp/Db/NDPluginBase.template","P=$(PREFIX),R=ZMQ1:,PORT=NDZMQ1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
 
 # Load all other plugins using commonPlugins.cmd
 #< $(AREA_DETECTOR)/iocBoot/commonPlugins.cmd
 
-set_requestfile_path("$(TOP)/slsDetectorApp/Db")
+set_requestfile_path(".")
+set_requestfile_path("$(slsDetector)/slsDetectorApp/Db")
+set_requestfile_path("$(ADCORE)/ADApp/Db")
 
 iocInit()
 
